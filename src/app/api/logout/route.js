1371d7bffdf9a2e6ed10a9632506367256ad02fd
darshil-2005 <darshilgandhi0620@gmail.com>
2026-07-04 01:20:25 +0530

    Restructuring

--- src/app/api/logout/route.js
+++
@@ -1,9 +0,0 @@
-export async function POST() {
-    return new Response(JSON.stringify({ message: 'Logged out' }), {
-      status: 200,
-      headers: {
-        'Set-Cookie': 'token=deleted; Path=/; Max-Age=0; HttpOnly',
-        'Content-Type': 'application/json'
-      }
-    });
-  }
